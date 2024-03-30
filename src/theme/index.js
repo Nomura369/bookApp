cf89c8a2670f8db307162c66e536c643f16aecfa
Jenny <jenny369314@gmail.com>
2024-03-30 14:46:52 +0800

    Theme設計

---
+++ src/theme/index.js
@@ -0,0 +1,13 @@
+import { DefaultTheme } from '@react-navigation/native';
+
+const BookTheme = {
+   ...DefaultTheme,
+   colors: {
+     ...DefaultTheme.colors,
+     purple: '#6200EE',
+     black: '#131313',
+     gray: '#666666',
+   },
+ };
+
+ export default MyTheme;
